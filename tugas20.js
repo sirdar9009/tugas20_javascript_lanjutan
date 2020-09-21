47c2c1e16ff4d05527310c2c37a9da5b5b29eb86
Muhammad Iqbal <71365593+sirdar9009@users.noreply.github.com>
2020-09-21 13:05:24 +0700

    Add files via upload

---
+++ tugas20.js
@@ -0,0 +1,4 @@
+var kata = "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable."
+
+console.log(kata.match("are"))
+console.log(kata.match("words"))
